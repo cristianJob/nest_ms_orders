1d143c5bba424fd930e766d0a1a91deac2bc486f
Cristian Carrillo <cristiancarrillo@Mac-mini-de-Cristian.local>
2025-11-25 21:34:05 -0300

    add changes

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,20 +7,18 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
-  PRODUCTS_MICROSERVICE_HOST: string;
-  PRODUCTS_MICROSERVICE_PORT: number;
+  NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi
   .object<EnvVars>({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
-    PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
-    PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true); // solo valida las variables definidas en el esquema
 
-const { error, value } = envVarsSchema.validate(process.env) as {
+const { error, value } = envVarsSchema.validate({ ...process.env, NATS_SERVERS: process.env.NATS_SERVERS?.split(',') }) as {
   value: EnvVars;
   error?: joi.ValidationError;
 };
@@ -33,6 +31,5 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
-  productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
-  productsMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
+  natsServers: envVars.NATS_SERVERS,
 };
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -5,7 +5,7 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { ChangeOrderStatusDto, OrderPaginationDto } from './dto';
-import { PRODUCT_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
@@ -14,7 +14,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('OrdersService');
 
   constructor(
-    @Inject(PRODUCT_SERVICE) private readonly productMicroservice: ClientProxy,
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy,
   ) {
     const connectionString = process.env.DATABASE_URL;
     const pool = new Pool({ connectionString });
@@ -33,7 +33,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
 
       // confirmar que los productos existen
       const productIds = createOrderDto.items.map(item => item.productId);
-      const products: any[] = await firstValueFrom(this.productMicroservice.send({ cmd: 'validate_products' }, productIds));
+      const products: any[] = await firstValueFrom(this.client.send({ cmd: 'validate_products' }, productIds));
 
       // calculo de los valores
       const totalAmount = createOrderDto.items.reduce((acc, orderItem) => {
@@ -129,7 +129,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     }
 
     const productIds = order.orderItems.map(item => item.productId);
-    const products: any[] = await firstValueFrom(this.productMicroservice.send({ cmd: 'validate_products' }, productIds));
+    const products: any[] = await firstValueFrom(this.client.send({ cmd: 'validate_products' }, productIds));
     return {
       ...order,
       orderItems: order.orderItems.map(item => ({
